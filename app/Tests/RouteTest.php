0ab35403d9bfc3022d1847d01cd9aa70808bb213
vladimir <singingcode@gmail.com>
2015-05-13 14:35:34 -0700

    initial commit

--- app/Tests/RouteTest.php
+++ app/Tests/RouteTest.php
@@ -11,37 +11,37 @@ use Vovanmix\CustomAdmin\Lib\Http\Request;
 class queryBuildTest extends \PHPUnit_Framework_TestCase
 {
 
-//    public function testUriWithId(){
-//        $Router = new Router();
-//        $Request = new Request();
-//        $Request->setInputServer([
-//            'REQUEST_URI' => '/category/edit/2/'
-//        ]);
-//        $Route = $Router->getRoute($Request);
-//
-//        $ExpectedRoute = new Route([
-//            'controller' => 'category',
-//            'action' => 'edit',
-//            'id' => '2',
-//        ]);
-//
-//        $this->assertEquals($ExpectedRoute, $Route);
-//    }
-//
-//    public function testUriWithIndex(){
-//        $Router = new Router();
-//        $Request = new Request();
-//        $Request->setInputServer([
-//            'REQUEST_URI' => '/category/'
-//        ]);
-//        $Route = $Router->getRoute($Request);
-//
-//        $ExpectedRoute = new Route([
-//            'controller' => 'category',
-//            'action' => 'index'
-//        ]);
-//
-//        $this->assertEquals($ExpectedRoute, $Route);
-//    }
+    public function testUriWithId(){
+        $Router = new Router();
+        $Request = new Request();
+        $Request->setInputServer([
+            'REQUEST_URI' => '/category/edit/2/'
+        ]);
+        $Route = $Router->getRoute($Request);
+
+        $ExpectedRoute = new Route([
+            'controller' => 'category',
+            'action' => 'edit',
+            'id' => '2',
+        ]);
+
+        $this->assertEquals($ExpectedRoute, $Route);
+    }
+
+    public function testUriWithIndex(){
+        $Router = new Router();
+        $Request = new Request();
+        $Request->setInputServer([
+            'REQUEST_URI' => '/category/'
+        ]);
+        $Route = $Router->getRoute($Request);
+
+        $ExpectedRoute = new Route([
+            'controller' => 'category',
+            'action' => 'index'
+        ]);
+
+        $this->assertEquals($ExpectedRoute, $Route);
+    }
 
 }
